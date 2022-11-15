7907b0c86f692b8c6f8b2bc7d38a74776c59dfba
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-15 13:09:40 +0100

    cicli in js

---
+++ script.js
@@ -0,0 +1,11 @@
+let listaspesa = ["latte","cereali","pane","pasta","insalata","frutta",]
+
+for(let i = 0; i<listaspesa.length; i++){
+    console.log(listaspesa[i])
+}
+
+let i=0
+while(i<listaspesa.length){
+    console.log(listaspesa[i])
+    i++
+}
