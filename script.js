319c3b76076e542f2a505fdd6f053f5e77a726e2
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-15 16:25:43 +0100

    esercizio finito

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+
+let listaspesa = ["latte", "cereali", "pane", "pasta", "insalata", "frutta",];
+
+/*
 let listaspesa = ["latte","cereali","pane","pasta","insalata","frutta",]
 
 for(let i = 0; i<listaspesa.length; i++){
-    console.log(listaspesa[i])
+    console.log(listaspesa[i]);
 }
 
 let i=0
 while(i<listaspesa.length){
-    console.log(listaspesa[i])
-    i++
+    console.log(listaspesa[i]);
+    i++;
 }
+
+*/
+
+
+let i=0
+while(i<listaspesa.length){
+
+    document.getElementById("ul").innerHTML +="<li>" + listaspesa[i] + "</li>";
+    i++;
+}
+
+
+
+
+
+
